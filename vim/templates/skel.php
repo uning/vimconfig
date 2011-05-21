45a7910afcadd91522dbc57f604d258cb3bf9119
hotel@ldev <hotel@ldev>
2011-05-21 11:17:39 +0800

    init from work

---
+++ vim/templates/skel.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * @FILE_EXT@
+ *
+ * Developed by @AUTHOR@ <@EMAIL@>
+ * Copyright (c) @YEAR@ @COMPANY@
+ * Licensed under terms of GNU General Public License.
+ * All rights reserved.
+ *
+ * Changelog:
+ * @DATE@ - created
+ *
+ */
+
+
+
---
+++ vim/templates/skel.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#
+# @FILE_EXT@
+#
+# Developed by @AUTHOR@ <@EMAIL@>
+# Copyright (c) @YEAR@ @COMPANY@
+# Licensed under terms of GNU General Public License.
+# All rights reserved.
+#
+# Changelog:
+# @DATE@ - created
+#
+
